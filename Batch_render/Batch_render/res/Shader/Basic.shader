84f1c0e3effe45b62afbc6b462df7535dbc54731
tan-shj <2079005390@qq.com>
2024-01-06 10:48:02 +0800

    坐标系统

--- Batch_render/Batch_render/res/Shader/Basic.shader
+++ Batch_render/Batch_render/res/Shader/Basic.shader
@@ -2,18 +2,18 @@
 #version 330 core
 
 layout(location = 0) in vec4 position;
-layout(location = 1) in vec4 color;
-layout(location = 2) in vec2 texCoord;
+//layout(location = 1) in vec4 color;
+layout(location = 1) in vec2 texCoord;
 
 out vec2 v_TexCoord;
-out vec4 ourcolor;
+//out vec4 ourcolor;
 
-uniform mat4 transform;
+uniform mat4 u_MVP;
 
 void main()
 {
-	gl_Position = transform * position;
-	ourcolor = color;
+	gl_Position = u_MVP * position;
+//	ourcolor = color;
 	v_TexCoord = texCoord;
 };
 
@@ -23,7 +23,7 @@ void main()
 out vec4 color;
 
 in vec2 v_TexCoord;
-in vec4 ourcolor;
+//in vec4 ourcolor;
 
 uniform sampler2D u_Texture1;
 uniform sampler2D u_Texture2;
@@ -31,5 +31,6 @@ uniform sampler2D u_Texture2;
 void main()
 {
 	vec4 texColor = mix(texture(u_Texture1, v_TexCoord), texture(u_Texture2, v_TexCoord), 0.2);
-	color = texColor * ourcolor;
+	//color = texColor * ourcolor;
+	color = texColor;
 };
--- Batch_render/Batch_render/src/main.cpp
+++ Batch_render/Batch_render/src/main.cpp
@@ -29,17 +29,61 @@ int main()
         return -1;
     }
 
+    glViewport(0, 0, 800, 600);
+    glEnable(GL_DEPTH_TEST);
+
     //x,y,z   vertex  color  texcoord
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
-         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
-         0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
-        -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
     GLuint indices[] = {
-        0, 1, 2, // 第一个三角形
-        2, 3, 0  // 第二个三角形
+        0,   1,  2,  3,  4,  5,
+        6,   7,  8,  9, 10, 11,
+        12, 13, 14, 15, 16, 17,
+        18, 19, 20, 21, 22, 23,
+        24, 25, 26, 27, 28, 29,
+        30, 31, 32, 33, 34, 35
     };
 
     GLCall(glEnable(GL_BLEND));//使能混合
@@ -51,20 +95,20 @@ int main()
     GLCall(glBindVertexArray(vao));
 
     VertexArray va;
-    VertexBuffer vb(vertices, 8 * 4 * sizeof(GLfloat));
+    VertexBuffer vb(vertices, 36 * 5 * sizeof(GLfloat));
 
     VertexBufferLayout layout;
     layout.Push<float>(3);//vertex
-    layout.Push<float>(3);//color
+    //layout.Push<float>(3);//color
     layout.Push<float>(2);//texcoord
     va.AddBuffer(vb, layout);
 
-    IndexBuffer ib(indices, 6);
+    IndexBuffer ib(indices, 36);
 
     Shader shader("res/Shader/Basic.shader");
     shader.Bind();
 
-    Texture texture1("res/Texture/OpenGL.jpg",GL_CLAMP_TO_EDGE);
+    Texture texture1("res/Texture/OpenGL.jpg", GL_REPEAT);
     Texture texture2("res/Texture/ChernoLogo.png",GL_REPEAT);
     texture1.Bind(0);
     shader.SetUniform1i("u_Texture1", 0);
@@ -82,7 +126,8 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
-        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f)); 
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         render.Clear();
         
         shader.Bind(); 
@@ -90,19 +135,33 @@ int main()
         texture2.Bind(1);
  
         {
-            glm::mat4 trans = glm::rotate(glm::mat4(1.0f), (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.0, 0.0, 1.0));//沿Z轴旋转90度/*glm::radians(90.0f)*/
-            //trans = glm::scale(trans, glm::vec3(1.0, 1.0, 1.0));//每个轴缩放倍数
-            trans = glm::translate(trans, glm::vec3(0.5f, 0.0f, 0.0f));//位移
-            shader.SetUniformMat4("transform", trans);
+            //glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1.0, 0.0, 0.0));//模型矩阵--旋转(rotate)、平移(translate)、缩放(scale)
+            glm::mat4 model = glm::rotate(glm::mat4(1.0f), (GLfloat)glfwGetTime() * glm::radians(-90.0f), glm::vec3(0.5, 1.0, 0.0));//模型矩阵--旋转(rotate)、平移(translate)、缩放(scale)
+            glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -5.0));//观察矩阵--相机位置；通过旋转、平移将世界坐标转换到观察空间--相机只能移动，不能旋转
+            glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);//投影矩阵--parameter->视野、宽高比、平截头体的近平面、平截头体的远平面
+            glm::mat4 MVP = proj * view * model;
+            shader.SetUniformMat4("u_MVP", MVP);
             render.Draw(va, ib, shader);
         }
 
-        {
-            glm::mat4 trans = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0.5f, 0.0f));//位移
-            trans = glm::scale(trans, glm::vec3(sin(glfwGetTime()), sin(glfwGetTime()), sin(glfwGetTime())));//每个轴缩放倍数
-            shader.SetUniformMat4("transform", trans);
+        //{
+            //glm::mat4 model = glm::rotate(glm::mat4(1.0f), (GLfloat)glfwGetTime() * glm::radians(-90.0f), glm::vec3(0.0, 1.0, 0.0));//模型矩阵--旋转(rotate)、平移(translate)、缩放(scale)
+            //glm::mat4 model = glm::rotate(glm::mat4(1.0f), (GLfloat)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.0, 1.0, 0.0));
+            //glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 0.0));
+            //glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+            //glm::mat4 MVP = proj * view * model;
+            //shader.SetUniformMat4("u_MVP", view);
+            //render.Draw(va, ib, shader);
+        //}
+
+       /* {
+            glm::mat4 model = glm::rotate(glm::mat4(1.0f), (GLfloat)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.0, 0.0, 1.0));
+            glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -3.0));
+            glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+            glm::mat4 MVP = proj * view * model;
+            shader.SetUniformMat4("u_MVP", MVP);
             render.Draw(va, ib, shader);
-        }
+        }*/
 
         glfwPollEvents();
         glfwSwapBuffers(window);
